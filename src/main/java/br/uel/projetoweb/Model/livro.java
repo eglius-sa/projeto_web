ff6d63c7bfe4f5744c5d0f5263f2793586ba1a56
eglius-sa <eglius.alexandre@uel.br>
2026-09-11 00:55:58 -0300

    esboco da classe livro

---
+++ src/main/java/br/uel/projetoweb/Model/livro.java
@@ -0,0 +1,33 @@
+package br.uel.projetoweb.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "livros")
+
+public class livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false, length = 150)
+    private String ISBN;
+    @Column(nullable = false, length = 100)
+    private String titulo;
+    @Column(nullable = false, length = 150)
+    private String autor;
+    /*Em outra tabela?
+    @Column
+    private Integer edicao;
+    @Column
+    private int ano;*/
+// Getters e setters
+}
